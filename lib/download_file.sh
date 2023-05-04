faf8a7a3a44eca07b2767c3b8dd5ae7a864aa63f
brianCollinsUI <brian@userinterviews.com>
2023-05-04 09:13:32 -0700

    Add source version namespacing for assets

--- lib/download_file.sh
+++ lib/download_file.sh
@@ -4,12 +4,12 @@ download_file () {
   local remote_path=$1
   local local_path=$2
 
-  curl "https://assets.userinterviews.com/${remote_path}" >> $build_dir/public/$local_path
+  curl "https://assets.userinterviews.com/${remote_path}" >> $build_dir/assets-$SOURCE_VERSION/public/$local_path
 
   local status=$?
 
   if [[ status -eq 0 ]]; then
-    echo "Downloaded remote file to $build_dir/public/$local_path" | indent
+    echo "Downloaded remote file to $build_dir/assets-$SOURCE_VERSION/public/$local_path" | indent
   fi
 
   return $status
