0c64eeb60830e9d8698bd2907d2d73ba099bd327
Kyle Shike <kyle.s@userinterviews.com>
2023-05-05 13:38:38 -0400

    exit 1 if manifests content type is xml

--- lib/download_file.sh
+++ lib/download_file.sh
@@ -4,6 +4,13 @@ download_file () {
   local remote_path=$1
   local local_path=$2
 
+  local content_type=$(curl "https://assets.userinterviews.com/${remote_path}" | grep -i "^Content-Type:")
+
+  if echo $content_type | grep -iq "application/xml"; then
+    echo "Invalid content type for manifest" | indent
+    return 1
+  fi
+
   curl "https://assets.userinterviews.com/${remote_path}" >> $build_dir/public/$local_path
 
   local status=$?
