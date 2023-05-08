cb9cfed9ab1d178246ba60053277127ddcc08e3c
Kyle Shike <kyle.s@userinterviews.com>
2023-05-08 16:31:09 -0400

    downloads zipped assets

--- lib/download_file.sh
+++ lib/download_file.sh
@@ -5,16 +5,16 @@ download_file () {
   local local_path=$2
   local file_url="https://assets.userinterviews.com/${remote_path}"
 
-  if curl -s -I $file_url | grep -iq "application/json"; then
-    curl $file_url >> $build_dir/public/$local_path
+  curl $file_url >> $build_dir/public/$local_path
 
-    local result=$?
+  echo "Downloaded remote file to $build_dir/public/$local_path" 
 
-    echo "Downloaded remote file to $build_dir/public/$local_path" | indent
+  unzip -q $build_dir/public/$local_path -d $build_dir/public/
+  rm $build_dir/public/$local_path
 
-    return 0
-  else
-    echo "Invalid content type for manifest" | indent
-    return 1
-  fi
+  echo "Unzipped assets to $build_dir/public" 
+
+  local result=$?
+
+  return 0
 }
